a1753df1d151e6232778702ddb00336a978812cb
Romain GUEDON <romain.guedon@eleves.ec-nantes.fr>
2018-05-02 16:18:09 +0100

    work in progress on algorithm1

--- PFA_algorithms.R
+++ PFA_algorithms.R
@@ -9,32 +9,45 @@
 # d, the number of principal components
 # X, one biological data type, see article
 
-algorithm_1 <- function(Xdata, d) {
+algorithm_1 <- function(X_i_dat) {
   #NOTE : the code is voluntary very explicite
   
-  X <- as.matrix(Xdata)
-  n <- dim(X)[2] #sample number
+  X_i <- as.matrix(X_i_dat)
+  n <- dim(X_i)[2] #sample number
   ones <- matrix(1, n, n) 
   I <- diag(1, n, n)
-  Xbar <- X %*% (I - (1/n)*ones)
-  eigs <- eigen(Xbar%*%t(Xbar), TRUE)
-  U <- eigs$vectors
-  values <- eigs$values #values already ordered
-  res <- list(U[1:d], values[1:d])
-  return(res)
+  X_i_bar <- X_i %*% (I - (1/n)*ones)
+  eigs <- eigen(X_i_bar%*%t(X_i_bar), TRUE)
+  
+  #let's find d_i
+  d_i <- 1
+  continue <- TRUE
+  sumEigen <- sum(eigs$values)
+  while(d_i <= dim(X_i)[1] && continue) {
+    #eigen values are ordered
+    if (sum(eigs$values[1:d_i])/sumEigen >= 0.8) {
+      continue <- FALSE
+    } else {d_i <- d_i + 1}
+  }
+  if (!continue) { #condition was satisfied
+    #compute Y_i 
+    #tmp <- as.matrix(eigs$vectors[,1:d_i])
+    Y_i <-  t(as.matrix(eigs$vectors[,1:d_i])) %*% X_i_bar
+    return(Y_i)
+    
+  } else {
+    print("Dimensions were not reduced")
+    # We still return a result ?
+    Y_i <- t(as.matrix(eigs$vectors[,1:d_i])) %*% X_i_bar
+    return(Y_i)
+  }
 }
 
+## TESTS ## 
 X <- as.matrix(data_list[[3]])
 d <- dim(X)[2]
-res <- algorithm_1(X, d)
+res <- algorithm_1(X)
 
-
-n=d
-ones <- matrix(1, n, n) 
-I <- diag(1, n, n)
-temp <- I - (1/n)%*%ones
-class(temp)
-Xbar <- X %*% temp
 
 
 ####Algorithm 2####
