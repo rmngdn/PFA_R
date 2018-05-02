780c57cff172d2fc386ca5c282fbb230888075a1
Romain GUEDON <romain.guedon@eleves.ec-nantes.fr>
2018-05-02 17:01:26 +0100

    algorithm 1 coded but find the eigen value seems to long for the gene expression data, because it has to find the eigen values of a 1.1 Gb matrix

--- PFA_algorithms.R
+++ PFA_algorithms.R
@@ -10,7 +10,8 @@
 # X, one biological data type, see article
 
 algorithm_1 <- function(X_i_dat) {
-  #NOTE : the code is voluntary very explicite
+  
+  # NOTE : the code is voluntary very explicite
   
   X_i <- as.matrix(X_i_dat)
   n <- dim(X_i)[2] #sample number
@@ -19,34 +20,35 @@ algorithm_1 <- function(X_i_dat) {
   X_i_bar <- X_i %*% (I - (1/n)*ones)
   eigs <- eigen(X_i_bar%*%t(X_i_bar), TRUE)
   
-  #let's find d_i
+  #let's find the min. number of eigen values/vectors d_i
   d_i <- 1
   continue <- TRUE
   sumEigen <- sum(eigs$values)
-  while(d_i <= dim(X_i)[1] && continue) {
-    #eigen values are ordered
-    if (sum(eigs$values[1:d_i])/sumEigen >= 0.8) {
-      continue <- FALSE
-    } else {d_i <- d_i + 1}
-  }
-  if (!continue) { #condition was satisfied
-    #compute Y_i 
-    #tmp <- as.matrix(eigs$vectors[,1:d_i])
-    Y_i <-  t(as.matrix(eigs$vectors[,1:d_i])) %*% X_i_bar
-    return(Y_i)
-    
-  } else {
-    print("Dimensions were not reduced")
-    # We still return a result ?
-    Y_i <- t(as.matrix(eigs$vectors[,1:d_i])) %*% X_i_bar
-    return(Y_i)
-  }
+  return(eigs)
+  # while(d_i <= n && continue) {
+  #   #eigen values are ordered
+  #   if (sum(eigs$values[1:d_i])/sumEigen >= 0.8) {
+  #     continue <- FALSE
+  #   } else {d_i <- d_i + 1}
+  # }
+  # if (!continue) { #condition was satisfied
+  #   #compute Y_i
+  #   #tmp <- as.matrix(eigs$vectors[,1:d_i])
+  #   Y_i <-  t(as.matrix(eigs$vectors[,1:d_i])) %*% X_i_bar
+  #   return(Y_i)
+  # 
+  # } else {
+  #   print("Dimensions were not reduced")
+  #   # We still return a result ?
+  #   Y_i <- t(as.matrix(eigs$vectors)) %*% X_i_bar
+  #   return(Y_i)
+  # }
 }
 
 ## TESTS ## 
-X <- as.matrix(data_list[[3]])
-d <- dim(X)[2]
-res <- algorithm_1(X)
+# X <- as.matrix(data_list[[3]])
+# d <- dim(X)[2]
+# res <- algorithm_1(X)
 
 
 
--- main_PFA.R
+++ main_PFA.R
@@ -2,7 +2,9 @@
 #DONT FORGET THE REFERENCES 
 #AND TO PACKAGE IT IN THE END
 
-source("./PFA_algorithms.R")
+pathToYourWD <- "~/Documents/Local_Work/PFA to R"
+setwd(pathToYourWD)
+source("PFA_algorithms.R")
 
 
 
@@ -10,9 +12,9 @@ source("./PFA_algorithms.R")
 
 # for example:
 # input:
-gene_fileName <- "data_gene_expression.csv"
-methy_fileName <- "data_methy_expression.csv"
-miRNA_fileName <- "data_mirna_expression.csv"
+gene_fileName <- "GLIO_Gene_Expression.txt"
+methy_fileName <- "GLIO_Methy_Expression.txt"
+miRNA_fileName <- "GLIO_Mirna_Expression.txt"
 fileName_list <- list(gene_fileName, 
                       methy_fileName, 
                       miRNA_fileName)
@@ -23,7 +25,19 @@ output_fileName <- "global_sample_spectrum.csv"
 ## Import the data:
 
 # for example
-data_list <- lapply(fileName_list, read.csv)
-
+data_list <- lapply(fileName_list, read.table)
 ## Capture the local sample-spectrum for each biological data type
+
+#local_Ys <- lapply(data_list, algorithm_1)
+
+Y2 <- algorithm_1(data_list[[2]])
+Y3 <- algorithm_1(data_list[[3]])
+#Y1 <- algorithm_1(data_list[[1]]) #too big
+
+## WHY IS IT SO LONG ? ##
+
+X <- as.matrix(data_list[[1]])
+n <- dim(X)[2]
+X_bar <- X %*% (diag(1,n,n) - (1/n)*matrix(1,n,n))
+R <- X_bar %*% t(X_bar) #1.1Gb
 
