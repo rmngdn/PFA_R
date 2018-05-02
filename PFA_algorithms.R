efacc7cb05957e9466b493159c10e244440aad97
Romain GUEDON <romain.guedon@eleves.ec-nantes.fr>
2018-05-02 15:33:10 +0100

    work in progress on algorithm1

--- PFA_algorithms.R
+++ PFA_algorithms.R
@@ -9,8 +9,32 @@
 # d, the number of principal components
 # X, one biological data type, see article
 
-algorithm_1 <- function(d, X) {
+algorithm_1 <- function(Xdata, d) {
+  #NOTE : the code is voluntary very explicite
   
+  X <- as.matrix(Xdata)
+  n <- dim(X)[2] #sample number
+  ones <- matrix(1, n, n) 
+  I <- diag(1, n, n)
+  Xbar <- X %*% (I - (1/n)*ones)
+  eigs <- eigen(Xbar%*%t(Xbar), TRUE)
+  U <- eigs$vectors
+  values <- eigs$values #values already ordered
+  res <- list(U[1:d], values[1:d])
+  return(res)
 }
+
+X <- as.matrix(data_list[[3]])
+d <- dim(X)[2]
+res <- algorithm_1(X, d)
+
+
+n=d
+ones <- matrix(1, n, n) 
+I <- diag(1, n, n)
+temp <- I - (1/n)%*%ones
+class(temp)
+Xbar <- X %*% temp
+
 
 ####Algorithm 2####
